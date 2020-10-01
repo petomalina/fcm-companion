32e0bbb49ba2fb245ca5224c67d6b4f140ba3398
Peter Malina <peter.malina@flowup.cz>
2020-10-01 16:26:22 +0200

    feat(serverutil): added ServeAll functionality that will make it easier to run tests

--- cmd/companion/main.go
+++ cmd/companion/main.go
@@ -3,27 +3,21 @@ package main
 import (
 	"context"
 	"github.com/blendle/zapdriver"
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/petomalina/fcm-companion/pkg/companion"
 	"github.com/petomalina/fcm-companion/pkg/serverutil"
-	"github.com/petomalina/xrpc/pkg/multiplexer"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"google.golang.org/grpc"
-	"net"
 	"os"
 )
 
 func main() {
 	ctx := context.Background()
-	requestedBind := ":" + os.Getenv("PORT")
 
-	// create the zap logger for future use
 	config := zapdriver.NewProductionConfig()
 	config.Level = zap.NewAtomicLevelAt(zapcore.DebugLevel)
 	logger, err := config.Build(zapdriver.WrapCore(
 		zapdriver.ReportAllErrors(true),
-		zapdriver.ServiceName("Echo"),
+		zapdriver.ServiceName("fcm-companion"),
 	))
 	if err != nil {
 		panic(err)
@@ -31,30 +25,10 @@ func main() {
 
 	svc, err := companion.New(ctx, os.Getenv("PROJECT_ID"), logger)
 	if err != nil {
-		logger.Fatal("An error occurred when initializing companion", zap.Error(err))
+		logger.Fatal("Cannot initialize companion", zap.Error(err))
 	}
 
-	lis, err := net.Listen("tcp", requestedBind)
-	if err != nil {
-		logger.Fatal("Cannot start listener", zap.Error(err))
+	if err := serverutil.ServeAll(ctx, logger, svc); err != nil {
+		logger.Fatal("Serving crashed", zap.Error(err))
 	}
-	bind := lis.Addr().(*net.TCPAddr).String()
-
-	// create and register the grpc server
-	grpcServer := grpc.NewServer()
-	gwmux := runtime.NewServeMux()
-
-	svc.Register(grpcServer)
-	err = svc.RegisterGateway(ctx, gwmux, bind, []grpc.DialOption{grpc.WithInsecure()})
-	if err != nil {
-		logger.Fatal("failed to register gateway endpoint handler", zap.Error(err))
-	}
-
-	serverutil.Serve(
-		lis,
-		logger,
-		multiplexer.GRPCHandler(grpcServer),
-		multiplexer.PubSubHandler(gwmux),
-		multiplexer.HTTPHandler(gwmux),
-	)
 }
--- pkg/serverutil/server.go
+++ pkg/serverutil/server.go
@@ -45,3 +45,48 @@ func Serve(lis net.Listener, logger *zap.Logger, handlers ...multiplexer.Handler
 		logger.Info("Exiting the FCM Companion", zap.Error(err))
 	}
 }
+
+func ServeAll(ctx context.Context, logger *zap.Logger, services ...interface{}) error {
+	lis, err := net.Listen("tcp", ":"+os.Getenv("PORT"))
+	if err != nil {
+		return err
+	}
+	// get the correct bind if the getenv returned an empty string
+	bind := lis.Addr().(*net.TCPAddr).String()
+
+	// create and register the grpc server
+	grpcServer, gwmux, err := MakeGRPCServerWithGateway(ctx, bind, services...)
+	if err != nil {
+		return err
+	}
+
+	Serve(
+		lis,
+		logger,
+		multiplexer.GRPCHandler(grpcServer),
+		multiplexer.PubSubHandler(gwmux),
+		multiplexer.HTTPHandler(gwmux),
+	)
+
+	return nil
+}
+
+func MakeGRPCServerWithGateway(ctx context.Context, bind string, services ...interface{}) (*grpc.Server, *runtime.ServeMux, error) {
+	grpcServer := grpc.NewServer()
+	gwmux := runtime.NewServeMux()
+
+	for _, svc := range services {
+		if s, ok := svc.(GRPCServer); ok {
+			s.Register(grpcServer)
+		}
+
+		if s, ok := svc.(GRPCGateway); ok {
+			err := s.RegisterGateway(ctx, gwmux, bind, []grpc.DialOption{grpc.WithInsecure()})
+			if err != nil {
+				return nil, nil, err
+			}
+		}
+	}
+
+	return grpcServer, gwmux, nil
+}
